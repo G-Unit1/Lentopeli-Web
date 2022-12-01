0724840462bb66774a32c8638f56d40afd1f0503
kimtuu <lofgrenkim@outlook.com>
2022-12-01 13:15:50 +0200

    added TODO for locations

--- website/flightscript.js
+++ website/flightscript.js
@@ -1,1 +1,9 @@
-'use strict';
+
+//Create map and load mapsoverlay from openstreetmap
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 8,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+//TODO: adding circles to available airport connections and
