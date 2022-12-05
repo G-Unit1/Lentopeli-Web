024106c5f67d829ab045cc089b9e355226491949
kim <lofgrenkim@outlook.com>
2022-12-06 00:00:36 +0200

    map maxBounds added

--- Website/flightscript.js
+++ Website/flightscript.js
@@ -2,11 +2,15 @@
 
 const map = L.map('map', {tap: false});
 L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-  maxZoom: 9,
+  maxZoom: 20,
+  minZoom: 3,
+  noWrap: true, // disables multiple side by side maps
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60.319120, 24.955821], 13);
-//TODO: adding circles to available airport connections and
+map.setView([60.319120, 24.955821], 9); //set start view
+map.setMaxBounds([[84.67351256610522, -174.0234375], [-75.995311187950925, 250.2421875]]); //Sets dragging borders so player cant lose sight of map
+
+//TODO: adding circles to available airport connections and player location
 
 //Guide modal code starts here
 let modal_button = document.getElementById('guide_modal');
