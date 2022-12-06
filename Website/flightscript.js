419541047ac2006ec0a0931b3eae7b93102c2a6a
kimtuu <lofgrenkim@outlook.com>
2022-12-06 14:34:46 +0200

    Added playerLocation placeholder

--- Website/flightscript.js
+++ Website/flightscript.js
@@ -3,12 +3,21 @@
 const map = L.map('map', {tap: false});
 L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
   maxZoom: 20,
-  minZoom: 3,
+  minZoom: 4,
   noWrap: true, // disables multiple side by side maps
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
 map.setView([60.319120, 24.955821], 9); //set start view
 map.setMaxBounds([[84.67351256610522, -174.0234375], [-75.995311187950925, 250.2421875]]); //Sets dragging borders so player cant lose sight of map
+
+// Player circle = red, available flights should = blue
+let playerLocation = L.circle([60.319120, 24.955821], { //Player location
+    color: 'red',
+    fillColor: 'red',
+    fillOpacity: 0.8,
+    radius: 10500
+}).addTo(map);
+playerLocation.bindPopup(`You are here`)
 
 //TODO: adding circles to available airport connections and player location
 
@@ -22,7 +31,7 @@ modal_button.addEventListener('click', function handleClick(evt) {
   evt.preventDefault();
 
   //debug console log
-  console.log('button pressed');
+  console.log('Guide button pressed');
 
   let modal = document.querySelector('dialog');
 
@@ -35,7 +44,7 @@ modal_button.addEventListener('click', function handleClick(evt) {
     btn.addEventListener('click', () => {
 
       //another debug console log
-      console.log('close modal');
+      console.log('close guide modal');
 
       modal.close();
     });
