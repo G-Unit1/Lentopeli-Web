9c32344bc847637dc5a8ed19bf1f2ccd3830c00d
Make <juhani.markus@gmail.com>
2022-12-08 19:54:27 +0200

    Pikku fix nopeesti

--- Flask/fly_to_airport.py
+++ Flask/fly_to_airport.py
@@ -2,18 +2,12 @@ import kursori
 
 
 def fly(username, airport):
-    try:
-        sql__update_player_location = f"update game set location = '{airport}' where screen_name = '{username}';"
-        kursori.kursori_func(sql__update_player_location)
+    sql__update_player_location = f"update game set location = '{airport}' where screen_name = '{username}';"
+    kursori.kursori_func(sql__update_player_location)
 
-        response = {
-            "value": "true",
-            "message": f"You just flew to {airport}"
-        }
+    response = {
+        "value": "true",
+        "message": f"You just flew to {airport}"
+    }
 
-        return response
-
-    except:
-        response = {
-            "value": "false"
-        }
+    return response
